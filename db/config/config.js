1d8ecfc5c65846ddd47d9e9a2498eec694c9ffd6
aldo okware <aldo.okware@moap.co.ug>
2019-11-16 18:28:44 +0300

    fix: postgres circle ci step up

--- db/config/config.js
+++ db/config/config.js
@@ -2,18 +2,23 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const host = process.env.PSQL_HOST
+const user = process.env.PSQL_USER
+const dbName = process.env.PSQL_DBNAME
+const password = process.env.PSQL_PASSWORD
+
 const config = {
   development: {
-    url: process.env.DB_DEVELOPMENT_URL,
+    url: `postgres://${user}:${password}@${host}:5432/${dbName}`
   },
   test: {
-    url: process.env.DB_TEST_URL,
+    url: `postgres://${user}:${password}@${host}:5432/${dbName}`,
   },
   staging: {
-    url: process.env.DB_STAGING_URL,
+    url: `postgres://${user}:${password}@${host}:5432/${dbName}`,
   },
   production: {
-    url: process.env.DB_PRODUCTION_URL,
+    url: `postgres://${user}:${password}@${host}:5432/${dbName}`,
   }
 };
 module.exports = config;
