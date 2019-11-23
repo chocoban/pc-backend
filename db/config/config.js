480c4ba354bcc282be42591a377c98a9d07686bb
Aldo Okware <aldo.okware@moap.co.ug>
2019-11-24 01:42:51 +0300

    fix: linting issues (#15)

--- db/config/config.js
+++ db/config/config.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const host = process.env.PSQL_HOST
-const user = process.env.PSQL_USER
-const dbName = process.env.PSQL_DBNAME
-const password = process.env.PSQL_PASSWORD
+const host = process.env.PSQL_HOST;
+const user = process.env.PSQL_USER;
+const dbName = process.env.PSQL_DBNAME;
+const password = process.env.PSQL_PASSWORD;
 
 const config = {
   development: {
